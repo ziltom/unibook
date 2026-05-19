16ee9d1a7c44ec4f7cc4429692790b1c878603a4
Caique Mendonça <132861061+Caique-Mendonca@users.noreply.github.com>
2026-05-19 17:59:59 -0300

    tentando

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,4 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.services) apply false
-    id("com.google.gms.google-services") version "4.4.4" apply false
 }
